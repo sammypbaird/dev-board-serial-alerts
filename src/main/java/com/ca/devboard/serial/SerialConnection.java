f71c43d46255ccbc5bc51578e336c6a9b1625992
gpete <greg.peterson@aggiemail.usu.edu>
2018-03-08 10:22:46 -0700

    Added auto port discovery

--- src/main/java/com/ca/devboard/serial/SerialConnection.java
+++ src/main/java/com/ca/devboard/serial/SerialConnection.java
@@ -5,86 +5,66 @@ import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
 import gnu.io.UnsupportedCommOperationException;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.TooManyListenersException;
 
 /**
- * Encapsulates all the logic needed to connect to the Arduino through
- * the serial port. You must call initialize before using it.
+ * Encapsulates all the logic needed to connect to the Arduino through the serial port. You must call initialize before
+ * using it.
  */
 public class SerialConnection
 {
 	/**
-	 * The port we're normally going to use.
-	 */
-	private static final String PORT_NAMES[] =
-	{
-		"/dev/tty.usbserial-A9007UX1", // Mac OS X
-		"/dev/ttyACM0", // Raspberry Pi
-		"/dev/ttyUSB0", // Linux
-		"COM6"
-	};
-	
-	/**
 	 * Milliseconds to block while waiting for port open
 	 */
 	private static final int TIME_OUT = 2000;
-	
+
 	public static SerialIO connect(int baudRate, SerialDataReceivedListener serialDataReceivedListener)
 	{
-	
-		CommPortIdentifier portId = null;
 		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
-
-		//First, Find an instance of serial port as set in PORT_NAMES.
+		List<CommPortIdentifier> foundSerialPorts = new ArrayList<>();
 		while (portEnum.hasMoreElements())
 		{
-			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-			for (String portName : PORT_NAMES)
+			CommPortIdentifier currPortId = (CommPortIdentifier)portEnum.nextElement();
+			if (currPortId.getPortType() != CommPortIdentifier.PORT_SERIAL)
+				continue;
+			System.out.println("Found serial port: " + currPortId.getName());
+			foundSerialPorts.add(currPortId);
+		}
+		for (CommPortIdentifier portId : foundSerialPorts)
+		{
+			try
 			{
-				if (currPortId.getName().equals(portName))
-				{
-					portId = currPortId;
-					break;
-				}
+				// open serial port, and use class name for the appName.
+				SerialPort serialPort = (SerialPort)portId.open(SerialConnection.class.getName(), TIME_OUT);
+				serialPort.disableReceiveTimeout();
+				serialPort.enableReceiveThreshold(1);
+				serialPort.notifyOnDataAvailable(true);
+
+				// set port parameters
+				serialPort.setSerialPortParams(baudRate,
+											   SerialPort.DATABITS_8,
+											   SerialPort.STOPBITS_1,
+											   SerialPort.PARITY_NONE);
+
+				// open the streams
+				BufferedReader input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+
+				SerialIO arduinoSerialIO = new SerialIO(serialPort, serialDataReceivedListener, input, serialPort.getOutputStream());
+				serialPort.addEventListener(arduinoSerialIO);
+				System.out.println("Connected to Port " + portId.getName());
+				return arduinoSerialIO;
+			}
+			catch (PortInUseException | UnsupportedCommOperationException | IOException | TooManyListenersException e)
+			{
+				System.out.println(String.format("Unable to connect to %s: %s", portId.getName(), e.getLocalizedMessage()));
 			}
 		}
-		if (portId == null)
-		{
-			throw new IllegalArgumentException("Could not find COM port!");
-		}
-		
-		System.out.println("Connected to Port " + portId.getName());
 
-		try
-		{
-			// open serial port, and use class name for the appName.
-			SerialPort serialPort = (SerialPort) portId.open(SerialConnection.class.getName(), TIME_OUT);
-			serialPort.disableReceiveTimeout();
-			serialPort.enableReceiveThreshold(1);
-			serialPort.notifyOnDataAvailable(true);
-
-			// set port parameters
-			serialPort.setSerialPortParams(baudRate,
-					SerialPort.DATABITS_8,
-					SerialPort.STOPBITS_1,
-					SerialPort.PARITY_NONE);
-
-			// open the streams
-			BufferedReader input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
-			
-			SerialIO arduinoSerialIO = new SerialIO(serialPort, serialDataReceivedListener, input, serialPort.getOutputStream());
-			serialPort.addEventListener(arduinoSerialIO);
-			return arduinoSerialIO;
-		}
-		catch (PortInUseException | UnsupportedCommOperationException | IOException | TooManyListenersException e)
-		{
-			throw new IllegalArgumentException(e.getMessage(), e);
-		}
+		throw new IllegalArgumentException("Could not any serial COM ports!");
 	}
-	
-	
 }
